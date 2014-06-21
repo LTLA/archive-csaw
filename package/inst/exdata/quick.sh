4b07c4c65b1269766a22140b05fe483ef5d9c567
alun <alun@3f7291af-367f-0410-92d8-b778034d9185>
2014-06-21 09:21:03 +0000

    Simplified exdata directory. Added script to automatically generate new single-end data.
    git-svn-id: https://svn.wehi.edu.au/svn/repos/SmythLab/ALun/csaw@1781 3f7291af-367f-0410-92d8-b778034d9185

--- package/inst/exdata/quick.sh
+++
@@ -1,8 +0,0 @@
-sam=$1
-bam=`echo $1 | sed "s/sam$/bam/"`
-sorted=`echo $bam | sed "s/.bam$//"`"_sort"
-samtools view -bS -o $bam $sam 
-samtools sort $bam $sorted
-samtools index $sorted".bam"
-rm $bam
-
