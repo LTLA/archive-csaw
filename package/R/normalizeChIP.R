285dc45232d281795a2320904e094077b71ae32a
alun <alun@3f7291af-367f-0410-92d8-b778034d9185>
2014-06-24 23:44:07 +0000

    Switched to warning when normalizeChIP called without specifying lib.size.
    git-svn-id: https://svn.wehi.edu.au/svn/repos/SmythLab/ALun/csaw@1804 3f7291af-367f-0410-92d8-b778034d9185

--- package/R/normalizeChIP.R
+++ package/R/normalizeChIP.R
@@ -1,62 +1,4 @@
-.normalizeChIP <- function(counts, lib.sizes=colSums(counts), refColumn=NULL, prior.count=0.1, logratioTrim = 0.3, sumTrim = 0.05)
-# This performs TMM normalization with some modifications to deal with low 
-# counts and other problems specific to ChIP-seq data. It also has a slight
-# difference in implementation to improve behaviour around ties.
-#
-# written by Aaron Lun
-# 7 October 2013
-{ 
-   	# Adding a prior count to bring (0, n) pairs into the trimming framework.
-	if (prior.count <= 0) { stop("prior count should be positive") }
-	expr<-cpm(counts, lib.size=lib.sizes, log=TRUE, prior.count=prior.count)
-
-	# Choosing the reference based on the most "middle" column (i.e. easiest to compare to all others).
-	if (is.null(refColumn)) { 
-		f<-apply(expr, 2, FUN=median)
-		refColumn<-which.min(abs(f-mean(f)))
-	}
-	if (sumTrim < 0 || sumTrim > 0.5 || logratioTrim < 0 || logratioTrim > 0.5) { 
-		stop("trimming proportions should lie between 0 and 0.5");
-	}
-
-	# Iterating across and using the trimmed mean of M-values.
-	nlibs<-ncol(counts)
-	normfacs<-numeric(nlibs)
-	for (lib in 1:nlibs) {
-		if (lib==refColumn) { next }
-	
-		# Discarding (0, 0) pairs with no information for a pairwise comparison. 
-		keep<-counts[,lib]!=0L & counts[,refColumn]!=0L
-		o<-expr[keep,lib]
-		r<-expr[keep,refColumn]
-		n<-sum(keep)
-		
-		# Trimming off extreme abundances with 'order' instead of 'rank', avoid issues with ties.
-		lo <- floor(n * sumTrim) + 1L
-		hi <- n + 1L - lo
-		a<-r+o
-		akeep<-logical(n)
-		akeep[order(a)[lo:hi]]<-TRUE
-		
-		# Trimming of extreme M-values.
-		lo <- floor(n * logratioTrim) + 1L
-		hi <- n + 1L - lo
-		m<-o-r
-		mkeep<-logical(n)
-		mkeep[order(m)[lo:hi]]<-TRUE
-
-		# Computing the trimmed mean of M-values. Precision weighting is not performed to avoid 
-		# upweighting high abundance bins which are more likely to be (differentially) enriched.
-		if (!any(mkeep & akeep)) { stop("trimming proportions are too aggressive") }
-		normfacs[lib]<-mean(m[mkeep & akeep])	
-	}
-
-	# Centering the normalization factors around a value of 1.
-	normfacs<-normfacs-mean(normfacs)
-	return(2^normfacs)
-}
- 
-normalizeChIP <- function(counts, lib.sizes=colSums(counts), type=c("scaling", "loess"), weighted=FALSE, dispersion=0.05, ...) 
+normalizeChIP <- function(counts, lib.sizes, type=c("scaling", "loess"), weighted=FALSE, dispersion=0.05, ...) 
 # This provides a wrapper to perform TMM normalization with non-standard
 # library sizes (e.g. due to filtering) and weighting turned off.
 # Alternatively, it can do a form a fast loess-like normalization which uses
@@ -66,6 +8,10 @@ normalizeChIP <- function(counts, lib.sizes=colSums(counts), type=c("scaling", "
 # written by Aaron Lun
 # 19 November, 2013
 {
+	if (missing(lib.sizes)) { 
+		lib.sizes  <- colSums(counts)
+		warning("library sizes not specified, column sums used instead")
+	}
 	type <- match.arg(type)
 	if (type=="scaling") { 
 		y<-DGEList(counts, lib.size=lib.sizes)
