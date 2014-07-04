38d0deff7da1294d237dda5838c780764b921204
alun <alun@3f7291af-367f-0410-92d8-b778034d9185>
2014-07-04 05:52:40 +0000

    Cleaned up comments for more clarity in cross-correlation C++ code.
    git-svn-id: https://svn.wehi.edu.au/svn/repos/SmythLab/ALun/csaw@1861 3f7291af-367f-0410-92d8-b778034d9185

--- package/src/correlate_reads.cpp
+++ package/src/correlate_reads.cpp
@@ -1,25 +1,28 @@
 #include "csaw.h"
 
-/* Stable running calculation of the variance and mean, using Welford's algorithm for both values
- * at decreasing delay distances (i.e. calculating values for the common regions at the maximum
- * shift, then iterating across the delay distances and computing running values). This approach
- * is more numerically stable than the sum of products method (see below).
+/* This function computes the mean and standard deviation of the count vectors
+ * across the genome, at every delay distance. This accounts for the genomic
+ * positions lost at the end of the chromosome (for the forward strand) or at
+ * the start of the chromosome (on the reverse strand) when shifting occurs.
  *
- * We also return the first delay distance which has a positive standard deviation. This is easier
- * than doing it outside, where we have to tangle with numerical imprecision.
+ * The function operates by computing the mean and variance of the 'core' positions,
+ * i.e., the positions that are present at the maximum delay distance. It then
+ * computes the running mean and variance when the delay is wound back, using 
+ * Welford's algorithm at every decreasing delay distance.
+ * 
+ * This approach is more numerically stable than the sum of products method to
+ * compute the variance (and the standard variance calculation can't adjust easily
+ * to changes in the mean). We also return the first delay distance which has a 
+ * non-zero standard deviation, to avoid division by zero later.
  */
 
 int fill (int n, std::deque<double>& mu, std::deque<double>& sd, const int* pos_ptr, const int* cnt_ptr,
 		const int total_len, const int chr_len, const bool reverse) {
-	/* Generating vectors. Also avoiding issues where 'n' is too large, by resetting the maximum delay
- 	 * shift so that there's at least two base positions. Otherwise, the variance is undefined.
- 	 * Note that we assume that the chromosome length is greater than 2.
- 	 */
 	mu.resize(n+1);
 	sd.resize(n+1);
-	if (n>chr_len-2) { n=chr_len-2; }
-	int first_pos_sd=-1;
+	if (n>chr_len-2) { n=chr_len-2; } // To ensure that 2 bases are available, so variance is estimable at max delay. 
 
+	// Changing parameters according to forward/reverse reads.
 	int step=1, start=0, end=total_len, threshold=chr_len-n;
 	if (reverse) { 
 		step=-1;
@@ -27,58 +30,52 @@ int fill (int n, std::deque<double>& mu, std::deque<double>& sd, const int* pos_
 		end=-1;
 		threshold=n+1;
 	}
-	/* Treading backwards, and identifying the first index where the position
- 	 * is past the threshold (in terms of increasing step, so we've got to +step
- 	 * after we reach the point of equality). This identifies the index of the
- 	 * first read outside the common subsection.
- 	 */
-	int temp_end=start;
+
+	// Find index of first read (going from end->start) with position <= threshold; adding 'step' to get endpoint for 'core' positions, i.e., from 'start' to 'temp_end'.
+	int temp_end=start; 
 	for (int i=end-step; i!=start-step; i-=step) {
 		if ((threshold - pos_ptr[i])*step >= 0) { 
 			temp_end=i+step;
 			break;
 		}
 	}
-
-	/* Computing the mean and variance for the longest common subsection (i.e.
- 	 * the largest delay).  We use a jump table for the latter, just to speed
- 	 * things up when we're dealing with a large number of zero's and one's.
- 	 * This is the typical case when you're running it with duplicates thrown
- 	 * out.
- 	 */
+	
+	// Computing the mean over the 'core' positions.
 	double& curmean=mu[n];
 	for (int index=start; index!=temp_end; index+=step) { curmean += cnt_ptr[index]; }
 	curmean/=chr_len-n;
-	double& curvar=sd[n];
+
+	// Computing the variance for the 'core' positions. Using a jump table to speed things up with lots of zero's and one's (e.g. dedupped data).
+	int first_pos_sd=-1;
 	std::deque<bool> present;
+	int present_counts=0;
 	{
-		std::deque<double> jumptab(1, -1);
+		double& curvar=sd[n];
+		std::deque<double> jumptab(2, R_NaReal); // Setting it up to a reasonable extent (need at least 0 and 1).
 		for (int index=start; index!=temp_end; index+=step) { 
 			const int& curcount=cnt_ptr[index];
-			if (int(jumptab.size()) <= curcount) { jumptab.resize(curcount+1, -1); } 
-			if (jumptab[curcount]<0) {
+			if (int(jumptab.size()) <= curcount) { jumptab.resize(curcount+1, R_NaReal); } 
+			if (ISNA(jumptab[curcount])) {
 				jumptab[curcount]=curcount-curmean;
 				jumptab[curcount]*=jumptab[curcount];
 			}
 			curvar += jumptab[curcount];
 		}
+		
+		// Adding the squared product for all the positions with zeroes.
 		const int num_zero_pos=chr_len-n-(temp_end-start)*step;
-		curvar+=curmean*curmean*num_zero_pos;
+		curvar+=curmean*curmean*num_zero_pos; 
 
-		/* Checking whether it would theoretically have a zero-sd, using integers. If there's 
- 		 * at least two unique positive integers, or there's one unique positive integer and at 
- 		 * least one zero value, then the standard deviation must be positive.
- 		 */
+		// Checking whether it would theoretically have a zero variance, i.e., need at least two different integers in cnt_ptr (including implicit zeros). 
 		present.resize(jumptab.size(), false);
-		int counted=0;
 		if (num_zero_pos) { 
-			++counted;
+			++present_counts;
 			present[0]=true;
 		}
 		for (size_t i=1; i<jumptab.size(); ++i) { 
-			if (jumptab[i]>0) { 
-				++counted; 
-				if (counted>=2) { 
+			if (!ISNA(jumptab[i])) { 
+				++present_counts; 
+				if (present_counts>=2) { 
 					first_pos_sd=n; 
 					break;
 				}
@@ -87,46 +84,53 @@ int fill (int n, std::deque<double>& mu, std::deque<double>& sd, const int* pos_
 		}
 	}
 	
-	/* Now, computing running mean and variance values for all the delays. We use Welford's algorithm
-	 * to provide some numerical stability when computing each value, in addition to being able to
-	 * be online with respect to decreasing delay distance (equivalent to adding a new base position
-	 * as the shift/delay distance decreases; equivalent to a new observation; need to handle a stream).
-	 */
-	int current_pos=threshold;
-	int current_count=0;
+	// Computing the running mean and variance with decreasing delay distance, using Welford's algorithm as the next base position comes online.
+	int current_pos=threshold,
+		current_index=temp_end,
+		current_count=0;
 	double delta;
 	for (int i=n-1; i>=0; --i) {
  		current_pos+=step;
-		if (temp_end!=end && pos_ptr[temp_end]==current_pos) {
-			current_count=cnt_ptr[temp_end];
-			temp_end+=step;
+		if (current_index!=end && pos_ptr[current_index]==current_pos) {
+			current_count=cnt_ptr[current_index];
+			current_index+=step;
 		} else { current_count=0; }
 
+		// Welford's algorithm [Technometrics, 4(3):419-420].
 		delta=current_count-mu[i+1];
 		mu[i]=mu[i+1]+delta/(chr_len-i);
 		sd[i]=sd[i+1]+delta*(current_count-mu[i]);
 		
-		/* Also checking whether this would break the zero-sd condition. Any addition
-		 * will be sufficient, as 'counted' should be at least 1 (i.e. at least two
-		 * non-negative values should be in there, somewhere). Note that this is 'first'
-		 * in terms of decreasing delay distance; it'll be the last delay distance for
-		 * which correlation calculations are valid.
+		/* Checking if the variance is zero, and if so, whether the current base position gets a non-zero variance.
+		 * Any addition will be sufficient, as 'counted' should be at least 1 by this point, if there is at least 1 read.
+ 	     * Note that this is 'first' in terms of decreasing delay distance; it'll be the largest delay distance for
+		 * which the standard deviation is positive.
 		 */
-		if (first_pos_sd < 0 && (current_count>=int(present.size()) || !present[current_count])) { 
-			first_pos_sd=i; }
+		if (first_pos_sd < 0) {
+			if (current_count>=int(present.size())) { present.resize(current_count+1, false); }
+			if (!present[current_count]) { 
+				present[current_count]=true;
+				++present_counts;
+			}
+			if (present_counts>2) { 
+				throw std::runtime_error("first delay distance with positive s.d. should already be assigned"); 
+			} else if (present_counts==2) {
+				first_pos_sd=i; 
+			}
+		}
 	}
 
-	// Tidying up the standard deviation estimates, to actually get the standard deviation.
+	// Obtaining the standard deviation.
 	for (int i=0; i<=n; ++i) {
 		sd[i]/=chr_len-i-1;
 		sd[i]=sqrt(sd[i]);
 	}
-
+	
 	return first_pos_sd;
 }
 
 /* A function to calculate the correlations between reads on the same chromosome.
- * Cross correlations can be calculated by supplying RLE value/lengths of forward reads 
+ * Cross-correlations can be calculated by supplying RLE value/lengths of forward reads 
  * as '1' and that of reverse reads as '2'. Autocorrelations can be calculated by 
  * supplying RLE value/lengths of all reads in both '1' and '2'.
  * 
@@ -171,10 +175,7 @@ SEXP correlate_reads (SEXP pos1, SEXP num1, SEXP pos2, SEXP num2, SEXP max_dist,
 		std::deque<double> sumfdiff(mdist+1), sumrdiff(mdist+1);
 		std::deque<int> nonempty(mdist+1);
 
-    	/* Now iterating through the valid forward/reverse combinations
-    	 * (where the reverse read must be downstream of the forward read).
-    	 * This is more efficient as we only have to consider the combinations present.
-    	 */
+    	// Looking at every forward/reverse combination, where the reverse read must be downstream of the forward read.
 		double rdiff, fdiff;
     	int f2rdex=0, f2rdex_copy;
     	for (int fdex=0; fdex<fLen; ++fdex) {
@@ -202,40 +203,39 @@ SEXP correlate_reads (SEXP pos1, SEXP num1, SEXP pos2, SEXP num2, SEXP max_dist,
         	}
     	}
 
-		/* Computing the actual correlation from the various collected statistics. The correlation is 
- 		 * defined as the sum of PRODUCT=(x-_X)(y-_Y) for all x and y. This can be written as 
- 		 * SUM[PRODUCT] for x>0,y>0 + SUM[PRODUCT] for x==0,y>0 + SUM[PRODUCT] for x>0,y==0 + SUM[PRODUCT] for x==0,y==0.
- 		 * The first expression is that in sum_ptr. The second is equal to -_X*( SUM[y-_Y] for x==0,y>0 )
- 		 *    = -_X*( -SUM[y-_Y] for x>0,y>0 - SUM[y-_Y] for x==0,y==0 - SUM[y-_Y] for x==0,y>0 )
- 		 *    = -_X*( -SUM[y-_Y] for x>0,y>0 - _Y*( # of times x==0 ) )
- 		 *  	where the first term inside the brackets is simply that of sumrdiff.
- 		 * The third expression, by similarity, is that of SUM[PRODUCT] for x>0,y==0
- 		 *    = -_Y*( -SUM[y-_Y] for x>0,y>0 - _X*( # of times y==0 ) )
- 		 *      where the first term inside the brackets is simply that of sumfdiff.
- 		 * And the last expression is simply _X*_Y*( # of times x==0 and y==0 ).
+		/* Computing the actual correlation from the various collected statistics. The correlation is defined as:
+		 *     sum of PRODUCT=(x-_X)(y-_Y) for all x and y
+		 *   = SUM[PRODUCT] for x>0,y>0 + SUM[PRODUCT] for x==0,y>0 + SUM[PRODUCT] for x>0,y==0 + SUM[PRODUCT] for x==0,y==0
+		 *
+ 		 * The first expression is that in sum_ptr. The second is equal to 
+		 *     -_X*( SUM[y-_Y] for x==0,y>0 )
+ 		 *   = -_X*( SUM[y-_Y] for all x, y - SUM[y-_Y] for x>0,y>0 - SUM[y-_Y] for x==0,y==0 - SUM[y-_Y] for x>0,y==0)
+ 		 *   = -_X*( 0 - SUM[y-_Y] for x>0,y>0 - SUM[y-_Y] for x==0,y==0 - SUM[y-_Y] for x>0,y==0)
+ 		 *   = -_X*( 0 - SUM[y-_Y] for x>0,y>0 + _Y*( # of times y==0 ) )
+		 *
+ 		 * where the first term inside the brackets is simply that of sumrdiff. The third expression, by similarity, is that of 
+		 *      SUM[PRODUCT] for x>0,y==0
+ 		 *    = -_Y*( - SUM[x-_X] for x>0,y>0 + _X*( # of times x==0 ) )
+		 *
+ 		 * where the first term inside the brackets is simply that of sumfdiff. And the last expression is simply 
+		 *      _X*_Y*( # of times x==0 and y==0 ).
  		 *
- 		 * When assembled, we get something like sum_ptr[i] + _X*sumrdiff[i] + _Y*sumfdiff[i]
- 		 *      - _X*_Y*( # of times x==0 + # of times y==0 - # of times both==0)
- 		 * The expression inside the brackets is simply the number of pairs
- 		 * where either is equal to zero; which, in turn, is equal to the total
- 		 * number of pairs without the number of pairs where both are non-zero.
+ 		 * When assembled, we get:
+  	   	 *      sum_ptr[i] + _X*sumrdiff[i] + _Y*sumfdiff[i] - _X*_Y*( # of times x==0 + # of times y==0 - # of times both==0)
+		 *
+ 		 * The expression inside the brackets is simply the number of pairs where either is equal to zero; which, in turn, is equal 
+		 * to the total number of pairs (after shifting by the delay) minus the number of pairs where both are non-zero.
  		 *
- 		 * The final subtraction is a bit numerically unstable, but if it done before the addition
- 		 * of '_X*sumrdiff[i] + _Y*sumfdiff[i]', it should be equivalent to straight summation
- 		 * where all x>0,y>0 pairs are in one stretch; all x==0,y==0 pairs in the next stretch; and
- 		 * all other pairs in the final stretch. It's not quite the worst case where all positive
- 		 * values are in one stretch, and all the negative values in another - then you'd be adding 
- 		 * and subtracting large values. The best case would be alternating positive and negative 
- 		 * values but that's harder to do, as you'd have to mess with sumptr additions, above.
+ 		 * Subtraction of the final term could be numerically unstable. This can't really be helped if speed is to be maintained.
+		 * In practice, it'll probably be okay because rmean and fmean should be fairly small (a lot more base positions than reads).
  		 */
 		for (int i=0; i<=mdist && i<=clen-2; ++i) {
-			sumptr[i]-=rmean[i]*fmean[i]*(clen-i-nonempty[i]); // A bit of numerical instability with this step.
-			sumptr[i]+=rmean[i]*sumfdiff[i]+fmean[i]*sumrdiff[i];
-		
-			// Providing some protection when sd is zero (e.g. empty post-shift vectors, delay is greater than the chromosome length). 
+			// Providing some protection for delay distances where the s.d. is zero.
 			if (i>ffirst || i>rfirst) {
 				sumptr[i]=0;
 			} else {
+				sumptr[i]-=rmean[i]*fmean[i]*(clen-i-nonempty[i]); // A bit of numerical instability with this step.
+				sumptr[i]+=rmean[i]*sumfdiff[i]+fmean[i]*sumrdiff[i];
 				sumptr[i]/=rsd[i]*fsd[i];
 				sumptr[i]/=clen-i-1; 
 			}
