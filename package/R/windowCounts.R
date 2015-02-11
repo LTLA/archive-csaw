0eb00059d972470a91455264a659aadc6ab51f72
alun <alun@3f7291af-367f-0410-92d8-b778034d9185>
2015-02-11 02:58:09 +0000

    Removed rescue.pairs references.
    git-svn-id: https://svn.wehi.edu.au/svn/repos/SmythLab/ALun/csaw@2302 3f7291af-367f-0410-92d8-b778034d9185

--- package/R/windowCounts.R
+++ package/R/windowCounts.R
@@ -77,7 +77,7 @@ windowCounts <- function(bam.files, spacing=50, width=spacing, ext=100, shift=0,
 				frag.start <- extended$start
 				frag.end <- extended$end
 			} else {
-				if (curpar$rescue.pairs) { 
+				if (!is.na(curpar$rescue.ext)) { 
 					out <- .rescuePE(bam.files[bf], where=where, param=curpar)
 				} else {
 					out <- .extractPE(bam.files[bf], where=where, param=curpar)
