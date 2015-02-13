ea78bc2ca1695883d9285b52bce63d0710b64fd4
alun <alun@3f7291af-367f-0410-92d8-b778034d9185>
2015-02-13 04:19:19 +0000

    Added len option for getWidths fragment length specification.
    git-svn-id: https://svn.wehi.edu.au/svn/repos/SmythLab/ALun/csaw@2315 3f7291af-367f-0410-92d8-b778034d9185

--- package/R/scaledAverage.R
+++ package/R/scaledAverage.R
@@ -12,7 +12,7 @@ scaledAverage <- function(y, scale=1, prior.count=NULL, ...)
 	aveLogCPM(y, prior.count=scale*prior.count, ...) - log2(scale)
 }
 
-getWidths <- function(data)
+getWidths <- function(data, len=NULL)
 # This computes the effective width of the data in the SummarizedExperiment
 # object. This is done by accounting for the effect of read extension; or, for
 # paired end data, the median fragment length (either from `rescue.ext`, or 
@@ -20,19 +20,33 @@ getWidths <- function(data)
 #
 # written by Aaron Lun
 # created 5 November 2014
-# last modified 9 February 2015
+# last modified 13 February 2015
 {
+	if (!is.null(len)) { len <- rep(len, length.out=ncol(data)) }
 	is.pe <- sapply(data$param, FUN=function(x) { x$pe=="both" })
 	frag.len <- integer(ncol(data))
+
+	# Single-end.
 	frag.len[!is.pe] <- data$final.ext[!is.pe]
+	missing.se <- is.na(frag.len[!is.pe])
+	if (any(missing.se)) {
+		if (is.null(len)) { 
+			stop("need to specify fragment lengths for single-end data")
+		}
+		frag.len[!is.pe][missing.se] <- len[!is.pe][missing.se]
+	}
 	
+	# Paired-end.
 	pe.len <- sapply(data$param, FUN=function(x) { x$rescue.ext })
 	not.def <- is.na(pe.len)
 	use.pe.len <- is.pe & !not.def
 	frag.len[use.pe.len] <- pe.len[use.pe.len]
 	use.def.len <- is.pe & not.def
 	if (any(use.def.len)) { 
-		frag.len[use.def.len] <- data$ext[use.def.len]	
+		if (is.null(len)) { 
+			stop("need to specify fragment lengths for paired-end data")
+		}
+		frag.len[use.def.len] <- len[use.def.len]	
 	}
 
 	frag.len <- as.integer(mean(frag.len))
