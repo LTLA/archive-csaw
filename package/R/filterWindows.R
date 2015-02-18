289fdc0897f280bbe96fb2b5618e9aba1f3637e1
alun <alun@3f7291af-367f-0410-92d8-b778034d9185>
2015-02-18 06:23:27 +0000

    Added filterWindows function to facilitate filtering.
    git-svn-id: https://svn.wehi.edu.au/svn/repos/SmythLab/ALun/csaw@2334 3f7291af-367f-0410-92d8-b778034d9185

---
+++ package/R/filterWindows.R
@@ -0,0 +1,55 @@
+filterWindows <- function(data, background, type="global", prior.count=2) 
+# This is a function for proportion- or background-based filtering of a
+# SummarizedExperiment object. For the former, it computes the relative ranks
+# that can be used to determine the proportion of highest-abundance windows to
+# keep. For the latter, it returns the enrichment term between data and
+# background.
+#
+# written by Aaron Lun
+# created 18 February 2015	
+{
+	type <- match.arg(type, c("global", "local", "control", "proportion"))
+	abundances <- scaledAverage(asDGEList(data), scale=1, prior.count=prior.count)
+
+	if (type=="proportion") {
+		spacing <- exptData(data)$spacing
+		if (is.null(spacing)) { stop("proportion-based filtering only works with window/bin counts") }
+		genome.windows <- sum(seqlengths(rowData(data))/spacing) # To account for those lost by filter>1 in windowCounts.
+		relative.rank <- 1 - (rank(abundances) - 1)/genome.windows
+		return(list(abundances=abundances, filter=relative.rank))
+
+	} else {
+		bwidth <- getWidths(background)
+		dwidth <- getWidths(data)
+		if (!identical(data$totals, background$totals)) { warning("data and background should be counted with the same parameters") }
+
+		if (type=="global") { 
+			relative.width <- median(bwidth)/median(dwidth)
+			bg.ab <- scaledAverage(asDGEList(background), scale=relative.width, prior.count=prior.count)
+			filter.stat <- abundances - median(bg.ab)
+			
+		} else if (type=="local") {
+ 		    if (!identical(nrow(data), nrow(background))) { stop("data and background should be of the same length") }	
+			relative.width <- (bwidth  - dwidth)/dwidth		
+			bg.y <- asDGEList(background)
+			bg.y$counts <- bg.y$counts - assay(data)
+
+			# Some protection for negative widths (counts should be zero, so only the prior gets involved in bg.ab).
+			subzero <- relative.width <= 0
+			if (any(subzero)) { 
+				relative.width[subzero] <- 1
+				bg.y$counts[sub.zero,] <- 0
+			}	
+			bg.ab <- scaledAverage(bg.y, scale=relative.width, prior.count=prior.count)
+			filter.stat <- abundances - bg.ab
+
+		} else {
+ 		    if (!identical(nrow(data), nrow(background))) { stop("data and background should be of the same length") }	
+			relative.width <- bwidth/dwidth
+			bg.ab <- scaledAverage(asDGEList(background), scale=relative.width, prior.count=prior.count)
+			filter.stat <- abundances - bg.ab
+		}
+
+		return(list(abundances=abundances, back.abundances=bg.ab, filter=filter.stat))
+	}
+}
