36ee188bbc882ca073d0daa7f02dcf80de69a5e4
alun <alun@3f7291af-367f-0410-92d8-b778034d9185>
2015-03-25 06:31:47 +0000

    Added checks to code.
    git-svn-id: https://svn.wehi.edu.au/svn/repos/SmythLab/ALun/csaw@2440 3f7291af-367f-0410-92d8-b778034d9185

--- package/R/upweightSummit.R
+++ package/R/upweightSummit.R
@@ -1,12 +1,15 @@
 upweightSummit <- function(ids, summits)
-# This takes a set of cluster IDs and a table produced by getBestTest
-# with by.pval=FALSE. It then computes relative weights for each cluster
-# ID, with the best entry upweighted by the number of tests.
+# This takes a set of cluster IDs and a vector of summits (e.g., from
+# getBestTest or from findMaxima). It then computes relative weights for each
+# cluster ID, with the best entry upweighted by the number of tests.
 #
 # written by Aaron Lun
 # created 26 February 2015
+# last modified 25 March 2015
 {
+	if (!is.integer(ids)) { ids <- as.integer(ids + 0.5) }
 	weights <- rep(1, length(ids))
+	if (any(ids <= 0L)) { stop("cluster ID vector should have positive integers") }
 	freq <- tabulate(ids)
 	sum.ids <- ids[summits]
 	freq.sum <- tabulate(sum.ids)
