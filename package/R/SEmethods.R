b8ceeef059e13ada38f1b2a4f1e7dfb188b96b55
alun <alun@3f7291af-367f-0410-92d8-b778034d9185>
2015-04-26 07:20:43 +0000

    Adjust manual specification of lib.sizes in SE methods, via the ellipsis; added warnings for missing library sizes.
    git-svn-id: https://svn.wehi.edu.au/svn/repos/SmythLab/ALun/csaw@2590 3f7291af-367f-0410-92d8-b778034d9185

--- package/R/SEmethods.R
+++ package/R/SEmethods.R
@@ -1,23 +1,41 @@
 # This defines some wrapper functions for various statistical methods 
 # in edgeR, for the SummarizedExperiment class. The idea is to relieve
 # the need for manual specification of the inputs every time; especially
-# for the library sizes.
+# for the library sizes (some care is required to still allow that).
 #
 # written by Aaron Lun
 # created 2 September 2014
+# last modified 26 April 2015
 
-setMethod("normalize", "SummarizedExperiment", function(object, lib.sizes=NULL, ...) {
-	if (is.null(lib.sizes)) { 
+setMethod("normalize", "SummarizedExperiment", function(object, ...) {
+	opt <- list(...)
+	is.libsize <- pmatch(names(opt), "lib.sizes")
+
+	if (all(is.na(is.libsize))) { 
 		if (is.null(object$totals)) {
-			return(normalizeCounts(counts=assay(object), ...))
-		} 
-		lib.sizes <- object$totals
+			warning("library sizes not found in 'totals'")
+		} else {
+			opt$lib.sizes <- object$totals
+		}
 	}
-	normalizeCounts(counts=assay(object), lib.sizes=lib.sizes, ...)
+
+	opt$counts <- assay(object)
+	do.call(normalizeCounts, opt)
 })
 
-setGeneric("asDGEList", function(object, lib.sizes=NULL, ...) { standardGeneric("asDGEList") })
-setMethod("asDGEList", "SummarizedExperiment", function(object, lib.sizes=NULL, ...) {
-	if (is.null(lib.sizes)) { lib.sizes <- object$totals }
-	DGEList(assay(object), lib.size=lib.sizes, ...)
+setGeneric("asDGEList", function(object, ...) { standardGeneric("asDGEList") })
+setMethod("asDGEList", "SummarizedExperiment", function(object, ...) {
+	opt <- list(...)
+	is.libsize <- pmatch(names(opt), "lib.size")
+
+	if (all(is.na(is.libsize))) { 
+		if (is.null(object$totals)) { 
+			warning("library sizes not found in 'totals'") 			
+		} else {
+			opt$lib.size <- object$totals
+		}
+	}
+
+	opt$counts <- assay(object)
+	do.call(DGEList, opt)
 })
