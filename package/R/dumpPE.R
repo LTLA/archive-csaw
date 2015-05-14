03c62fd056c0622763350672db9e2d05d0f6e038
alun <alun@3f7291af-367f-0410-92d8-b778034d9185>
2015-05-14 01:58:31 +0000

    Added more info to dumped PE file.
    git-svn-id: https://svn.wehi.edu.au/svn/repos/SmythLab/ALun/csaw@2660 3f7291af-367f-0410-92d8-b778034d9185

--- package/R/dumpPE.R
+++ package/R/dumpPE.R
@@ -6,6 +6,7 @@ dumpPE <- function(bam.file, prefix, param=readParam(pe="both"), overwrite=FALSE
 #
 # written by Aaron Lun
 # created 14 February 2015
+# last modified 14 May 2015
 {
 	if (param$pe!="both") { stop("paired-end inputs required") }
 	extracted.chrs <- .activeChrs(bam.file, param$restrict)
@@ -17,38 +18,91 @@ dumpPE <- function(bam.file, prefix, param=readParam(pe="both"), overwrite=FALSE
 	ohandle <- file(ofile, open='w')
 	write.table(file=ohandle, data.frame("@SQ", paste0("SN:", names(extracted.chrs)),
  		paste0("LN:", extracted.chrs)), sep="\t", quote=FALSE, col.names=FALSE, row.names=FALSE)	
+	write.table(file=ohandle, data.frame("@PG", "ID:dumped", dump.prog.name),
+		sep="\t", quote=FALSE, col.names=FALSE, row.names=FALSE, append=TRUE)	
 
+	# Paired, properly paired, forward strand, mate is reverse strand.
+	active.flag <- 0x1 + 0x2 + 0x20 + 0x40 
 	counter <- 0L
-	active.flag <- 0x1 + 0x2 + 0x20 + 0x40 # Paired, properly paired, forward strand, mate is reverse strand.
+	all.seq <- all.qual <- character(0)
 
     for (i in 1:length(extracted.chrs)) {
 		chr <- names(extracted.chrs)[i]
 		where <- GRanges(chr, IRanges(1L, extracted.chrs[i]))
+		out <- .getPairedEnd(bam.file, where=where, param=param, with.reads=TRUE)
 
-        if (.rescueMe(param)) { 
-			out <- .rescuePE(bam.file, where=where, param=param)
-		} else {
-			out <- .extractPE(bam.file, where=where, param=param)
+		# Setting up width data.
+		all.cig <- all.end <- all.names <- list()
+		all.widths <- list()
+		index <- 1L
+		nl <- length(out$left$pos)
+		if (nl) { 
+			all.cig[[index]] <- paste0(out$left$qwidth, "M")
+			all.end[[index]] <- out$right$pos		
+			all.names[[index]] <- paste0("paired:", counter+1:nl, ":", out$right$qwidth)
+			all.widths[[index]] <- out$left$qwidth
+			index <- index + 1L
+			counter <- counter + nl
 		}
+		nr <- length(out$rescued$pos)
+		if (nr) { 
+			all.cig[[index]] <- paste0(out$rescued$qwidth, "M")
+			all.end[[index]] <- out$rescued$pos
+			all.names[[index]] <- paste0("rescued:", counter+1:nr, ":", out$rescued$strand)
+			all.widths[[index]] <- out$rescued$qwidth
+			counter <- counter + nr
+		}	
 
 		if (length(out$pos)) {
-			fids <- paste0("f", counter + 1:length(out$pos))
-			fullsize <- out$size
-			ending <- out$pos + out$size - 1L
-			subzero <- out$pos <= 0L # Shorten rescued fragment if it runs off the front (negative positions are not stored).
+			all.cig <- unlist(all.cig)
+			all.end <- unlist(all.end)
+			all.names <- unlist(all.names)
+			all.widths <- unlist(all.widths)
+
+			# Setting up sequence, quality strings.
+			ref.widths <- which(tabulate(all.widths)!=0L)
+			if (any(is.na(all.seq[ref.widths]))) {
+				for (w in ref.widths) {
+					all.seq[w] <- paste(rep("N", w), collapse="")
+					all.qual[w] <- paste(rep("#", w), collapse="")
+				}
+			}	
+
+			# Shorten rescued fragment if it runs off the front (negative positions are not stored).
+			subzero <- out$pos <= 0L 
 			if (any(subzero)) { 
+				ending <- out$pos[subzero] + out$size[subzero] - 1L
 				out$pos[subzero] <- 1L
-				fullsize[subzero] <- ending[subzero] 
+				out$size[subzero] <- ending 
 			}
 
-			write.table(file=ohandle, data.frame(fids, active.flag, chr, out$pos, 255, "1M", "=", 
-				ending, fullsize, "N", "#"), sep="\t", quote=FALSE, col.names=FALSE, 
-				row.names=FALSE, append=TRUE)
+			write.table(file=ohandle, data.frame(all.names, active.flag, chr, out$pos, 255, 
+				all.cig, "=", all.end, out$size, all.seq[all.widths], all.qual[all.widths]),
+				sep="\t", quote=FALSE, col.names=FALSE, row.names=FALSE, append=TRUE)
 		}	
-		counter <- counter + length(out$pos)
 	}
 
 	close(ohandle)
 	output <- suppressWarnings(asBam(ofile, prefix, indexDestination=TRUE, overwrite=overwrite))
 	return(invisible(output))
 }
+
+# Assorted helper functions for use elsewhere.
+
+dump.prog.name <- "PN:csaw::dumpPE"
+.isDumpedBam <- function(bam.file) {
+	return(dump.prog.name %in% scanBamHeader(bam.file, what="text")[[1]]$text[["@PG"]])
+}
+
+.isRescuedRead <- function(qnames) {
+	return(grepl("^rescued:", qnames))
+}
+
+.getMateWidth <- function(qnames) { 
+	return(as.integer(sub("^.*:", "", qnames)))
+}
+
+.getRescueStr <- function(qnames) {
+	return(sub("^.*:", "", qnames))
+}
+
