61f3314c87e6cf7a6648a460cb1000fcb630fd6e
Aaron Lun <alun@wehi.edu.au>
2015-12-19 15:00:11 +0000

    Switched to macro for C++ registration.

--- package/src/init.cpp
+++ package/src/init.cpp
@@ -1,27 +1,28 @@
 #include "csaw.h"
+#define REGISTER(x, i) {#x, (DL_FUNC) &x, i}
 
 extern "C" {
 
 static const R_CallMethodDef all_call_entries[] = {
-	{"collate_exon_data", (DL_FUNC) &collate_exon_data, 4},
-	{"annotate_overlaps", (DL_FUNC) &annotate_overlaps, 13},
-	{"best_in_cluster", (DL_FUNC) &best_in_cluster, 3},
-	{"get_cluster_stats", (DL_FUNC) &get_cluster_stats, 6},
-	{"merge_windows", (DL_FUNC) &merge_windows, 6},
-	{"correlate_reads", (DL_FUNC) &correlate_reads, 6},
-	{"get_rle_counts", (DL_FUNC) &get_rle_counts, 5}, 
-	{"get_profile", (DL_FUNC) &get_profile, 6}, 
-	{"find_maxima", (DL_FUNC) &find_maxima, 5}, 
-	{"check_bimodality", (DL_FUNC) &check_bimodality, 5}, 
-	{"extract_pair_data", (DL_FUNC) &extract_pair_data, 8}, 
-	{"get_leftovers", (DL_FUNC) &get_leftovers, 3}, 
-	{NULL, NULL, 0}
+    REGISTER(collate_exon_data, 4),
+    REGISTER(annotate_overlaps, 13),
+    REGISTER(best_in_cluster, 3),
+    REGISTER(get_cluster_stats, 6),
+    REGISTER(merge_windows, 6),
+    REGISTER(correlate_reads, 6),
+    REGISTER(get_rle_counts, 5), 
+    REGISTER(get_profile, 6), 
+    REGISTER(find_maxima, 5), 
+    REGISTER(check_bimodality, 5), 
+    REGISTER(extract_pair_data, 8), 
+    REGISTER(get_leftovers, 3), 
+    {NULL, NULL, 0}
 };
 
 void attribute_visible R_init_csaw(DllInfo *dll) {
-	R_registerRoutines(dll, NULL, all_call_entries, NULL, NULL);
-	R_useDynamicSymbols(dll, FALSE);
-	R_forceSymbols(dll, TRUE);
+    R_registerRoutines(dll, NULL, all_call_entries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+    R_forceSymbols(dll, TRUE);
 }
 
 }
