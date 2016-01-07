4ba64da4731060a77f2616e5d47a0236fb75715a
LTLA <alun@wehi.edu.au>
2016-01-07 23:43:29 +0000

    Streamlined behaviour of consolidateSizes.

--- R/consolidateSizes.R
+++ R/consolidateSizes.R
@@ -1,27 +1,37 @@
 consolidateSizes <- function(data.list, result.list, equiweight=TRUE, 
-    merge.args=list(tol=1000), combine.args=list(), region=NULL, overlap.args=list()) 
+    merge.args=list(), combine.args=list(), region=NULL, overlap.args=list()) 
 # Consolidates results for multiple window sizes into a result for the
 # genomic region over which those windows are tiled. Returns the combined
 # results, as well as ID vectors for cross-referencing and inspection.
 #
 # written by Aaron Lun
 # created 26 February 2015
-# last modified 22 July 2015
+# last modified 8 January 2016
 {
 	nset <- length(data.list)
 	set.it.vec <- seq_len(nset)
 	if (nset!=length(result.list)) { stop("data list must have same length as result list") }
+
 	for (x in set.it.vec) {
-		currows <- nrow(data.list[[x]])
+        current <- data.list[[x]]
+        if (is(current, "RangedSummarizedExperiment")) { 
+            data.list[[x]] <- current <- rowRanges(current) 
+        } else if (!is(current, "GenomicRanges")) {
+            stop("elements of data list must be RSE or GRanges objects")
+        }
+
+		currows <- length(current)
 		ntab <- nrow(result.list[[x]])
 		if (currows!=ntab) { stop("corresponding entries of data and result lists must have same number of entries") }
 	}
 
 	# Merging windows, or finding overlaps.
 	if (is.null(region)) { 
-		all.ranges <- list()
-		for (x in set.it.vec) { all.ranges[[x]] <- rowRanges(data.list[[x]]) }
-		all.ranges <- do.call(c, all.ranges)
+		all.ranges <- do.call(c, data.list)
+        if (is.null(merge.args$tol)) { 
+            merge.args$tol <- 100
+            warning("'tol' for 'mergeWindows' set to a default of 100 bp")
+        }
 		merged <- do.call(mergeWindows, c(merge.args, regions=all.ranges)) 
 
 		# Formatting for nice output.
@@ -32,19 +42,18 @@ consolidateSizes <- function(data.list, result.list, equiweight=TRUE,
 			final.ids[[x]] <- merged$id[last+seq_len(currows)]
 			last <- last + currows
 		}
-		names(final.ids) <- names(data.list)
 	} else {
 		all.ranges <- list()
 		final.ids <- list()
 		for (x in set.it.vec) {
-			olap <- do.call(findOverlaps, c(query=region, subject=rowRanges(data.list[[x]]), overlap.args))
+			olap <- do.call(findOverlaps, c(query=region, subject=data.list[[x]], overlap.args))
 			final.ids[[x]] <- olap
 			all.ranges[[x]] <- queryHits(olap)
 			result.list[[x]] <- result.list[[x]][subjectHits(olap),]
 		}
 		merged <- list(id=unlist(all.ranges), region=region)
-		names(final.ids) <- names(data.list)
 	}
+	names(final.ids) <- names(data.list)
 
 	# Calculating weights, so each window size (or spacing) has the same contribution to the final outcome.
 	if (equiweight) {
