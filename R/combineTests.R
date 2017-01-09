8ea9e66cf0069c7283a63e2cf744681c040328f8
Aaron Lun <alun@wehi.edu.au>
2017-01-09 15:18:15 +0000

    Refactored some combining code, docs.

--- R/combineTests.R
+++ R/combineTests.R
@@ -7,33 +7,24 @@ combineTests <- function(ids, tab, weight=NULL, pval.col=NULL, fc.col=NULL)
 # 
 # written by Aaron Lun
 # created 30 July 2013
-# last modified 8 January 2017
+# last modified 9 January 2017
 {
     input <- .check_test_inputs(ids, tab, weight)
     ids <- input$ids
     tab <- input$tab
     groups <- input$groups
     weight <- input$weight
-
-	# Saying which columns have the log-fold change field.
-	if (is.null(fc.col)) { 
-		fc.col <- grep("logFC", colnames(tab))
-	} else if (is.character(fc.col)) { 
-        fc.col <- match(fc.col, colnames(tab)) 
-        if (any(is.na(fc.col))) { stop("failed to match logFC column names") }
-    }
-    fc.col <- as.integer(fc.col) - 1L
-
-	# Saying which column is the p-value field.
-    is.pval <- .getPValCol(pval.col, tab) - 1L
  
 	# Running the clustering procedure.
-	out <- .Call(cxx_get_cluster_stats, fc.col, is.pval, tab, ids, weight, 0.5)
+    fc.col <- .parseFCcol(fc.col, tab) 
+    is.pval <- .getPValCol(pval.col, tab)
+	out <- .Call(cxx_get_cluster_stats, fc.col - 1L, is.pval - 1L, tab, ids, weight, 0.5)
 	if (is.character(out)) { stop(out) }
+
 	combined <- data.frame(out[[1]], out[[2]], out[[3]], p.adjust(out[[3]], method="BH"), row.names=groups)
 	colnames(combined) <- c("nWindows", 
-			sprintf("%s.%s", rep(colnames(tab)[fc.col+1L], each=2), c("up", "down")), 
-			colnames(tab)[is.pval+1L], "FDR")
+			sprintf("%s.%s", rep(colnames(tab)[fc.col], each=2), c("up", "down")), 
+			colnames(tab)[is.pval], "FDR")
 
     # Adding direction.
     if (length(fc.col)==1L) {
@@ -73,4 +64,17 @@ combineTests <- function(ids, tab, weight=NULL, pval.col=NULL, fc.col=NULL)
         originals <- id.order
     }
     return(list(ids=ids, groups=all.names, tab=tab, weight=weight, original=originals))
+}
+
+.parseFCcol <- function(fc.col, tab, multiple=TRUE) {
+    if (is.null(fc.col)) { 
+        fc.col <- grep("logFC", colnames(tab))
+    } else if (is.character(fc.col)) { 
+        fc.col <- match(fc.col, colnames(tab)) 
+        if (any(is.na(fc.col))) { stop("failed to match logFC column names") }
+    }
+    if (!multiple && length(fc.col)!=1L) {
+        stop("only one column should be specified by 'fc.col'")
+    }
+    as.integer(fc.col)
 }
--- R/empiricalFDR.R
+++ R/empiricalFDR.R
@@ -5,37 +5,28 @@ empiricalFDR <- function(ids, tab, weight=NULL, pval.col=NULL, fc.col=NULL, neg.
 #
 # written by Aaron Lun
 # created 7 January 2017
-# last modified 8 January 2017
+# last modified 9 January 2017
 {
-    if (length(fc.col)==0L) { 
-        fc.col <- grep("logFC", colnames(tab))
-    }
-    if (length(fc.col)!=1L) {
-        stop("only one column should be specified by 'fc.col'")
-    }
-    cur.fc <- tab[,fc.col]
-    if (neg.down) {
-        wrong.dir <- cur.fc < 0
-    } else {
-        wrong.dir <- cur.fc > 0
-    }
-    
+    fc.col <- .parseFCcol(fc.col, tab, multiple=FALSE)
     pval.col <- .getPValCol(pval.col, tab)
     pval.colname <- colnames(tab)[pval.col]
+    
+    all.p <- .make_one_sided(tab, pval.col=pval.col, fc.col=fc.col)
+    if (neg.down) { 
+        all.p <- list(right=all.p$up, wrong=all.p$down)
+    } else {
+        all.p <- list(right=all.p$down, wrong=all.p$up)
+    }
 
-    # Converting to one-sided p-values and combining.
-    new.p <- tab[,pval.col]/2
-    new.p[wrong.dir] <- 1 - new.p[wrong.dir]
+    # Combining one-sided p-values.
     right.tab <- tab
-    right.tab[,pval.col] <- new.p
+    right.tab[,pval.col] <- all.p$right
     right.com <- combineTests(ids, right.tab, weight=weight, pval.col=pval.col, fc.col=fc.col)
     right.com$direction <- NULL
     
-    # Repeating in the other direction (calculating 'new.p' fresh, to avoid numeric imprcesion from repeated "1-" ops).
-    new.p <- tab[,pval.col]/2
-    new.p[!wrong.dir] <- 1 - new.p[!wrong.dir]
+    # Repeating in the other direction.
     wrong.tab <- tab
-    wrong.tab[,pval.col] <- new.p
+    wrong.tab[,pval.col] <- all.p$wrong
     wrong.com <- combineTests(ids, wrong.tab, weight=weight, pval.col=pval.col, fc.col=integer(0))
 
     # Computing empirical FDR.
@@ -51,5 +42,19 @@ empiricalFDR <- function(ids, tab, weight=NULL, pval.col=NULL, fc.col=NULL, neg.
     right.com$FDR <- empirical
     return(right.com)
 }
+
+.make_one_sided <- function(tab, pval.col, fc.col) {
+    cur.fc <- tab[,fc.col]
+    going.up <- cur.fc > 0
+    pval <- tab[,pval.col]
+    
+    # Calculating each set fresh, to avoid numeric imprcesion from repeated "1-" operations
+    up.p <- pval/2
+    up.p[!going.up] <- 1 - up.p[!going.up]
+    down.p <- pval/2
+    down.p[going.up] <- 1 - down.p[going.up]
+    return(list(up=up.p, down=down.p))
+}
+
 
 
